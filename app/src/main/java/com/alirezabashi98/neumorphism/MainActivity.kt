c16cb587e6d343e056c7338cbe4e402ceca496fa
alirezabashi98 <alirezabashi98@gmail.com>
2022-07-10 13:27:06 +0430

    There is a neomorphism practice project that I added to the library and there is also a test page. Now I will delete it.

---
+++ app/src/main/java/com/alirezabashi98/neumorphism/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alirezabashi98.neumorphism
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
